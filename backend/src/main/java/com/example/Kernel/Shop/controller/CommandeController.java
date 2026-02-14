cf5f2044f030befe3feeb0bba06c5a03d66c8088
djeffalKhaled <djeffalkhaledfaiz@gmail.com>
2026-02-14 14:10:52 +0100

    CommandController changes

--- backend/src/main/java/com/example/Kernel/Shop/controller/CommandeController.java
+++ backend/src/main/java/com/example/Kernel/Shop/controller/CommandeController.java
@@ -1,7 +1,9 @@
 package com.example.Kernel.Shop.controller;
 
 import com.example.Kernel.Shop.entity.Commande;
+import com.example.Kernel.Shop.entity.Product;
 import com.example.Kernel.Shop.repository.CommandeRepository;
+import com.example.Kernel.Shop.repository.ProductRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,20 +13,34 @@ import org.springframework.web.server.ResponseStatusException;
 import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 
 @RestController
 @RequestMapping("/api/commandes")
 public class CommandeController {
     private final CommandeRepository commandeRepository;
+    private final ProductRepository productRepository;
 
-    public CommandeController(CommandeRepository commandeRepo) {
+    public CommandeController(CommandeRepository commandeRepo, ProductRepository productRepo) {
         this.commandeRepository = commandeRepo;
+        this.productRepository = productRepo;
     }
 
     @PostMapping
     public Commande createCommande(@RequestBody Commande commande) {
         commande.setDateCommande(new Date());
+        List<Commande.Item> items = commande.getItems();
+        for (int i = 0; i < items.size(); i++) {
+            String productId = items.get(i).getProductId();
+            Product product = productRepository.findById(productId)
+                           .orElseThrow(() -> new RuntimeException("Product not found"));
+            if (product != null) {
+                int stock = product.getStock();
+                product.setStock(stock - items.get(i).getQuantity());
+                productRepository.save(product);
+            }
+        }
         return commandeRepository.save(commande);
     }
 
