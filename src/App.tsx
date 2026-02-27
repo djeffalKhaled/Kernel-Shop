da8778db4b657d4c8423c9255cc777b0656bb5b7
djeffalKhaled <djeffalkhaledfaiz@gmail.com>
2026-02-27 17:30:26 +0100

    merging

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,9 @@
 import "./styles/App.css"
-import NavBar from './component/NavBar'
-import Signup from './component/SignupLogin'
-import Product from './component/Product'
-import CategorieBar from "./component/CategorieBar"
-import ProductList from "./component/ProductList"
-import { useState } from "react"
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom"
-import AddProduct from "./component/AddProduct"
-import NavBarSupplier from "./component/NavBarSupplier"
 import ClientPage from "./component/ClientPage"
+import SupplierPage from "./component/SupplierPage"
 
 function App() {
-
-    // need to add support for supplier page
-    function SupplierPage() {
-        return (
-            <>
-            <NavBarSupplier></NavBarSupplier>
-            </>
-        )
-    }
 
     return (
         <BrowserRouter>
